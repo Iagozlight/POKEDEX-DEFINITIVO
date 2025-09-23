235d4f669be4e4454b1cfd6f803f4d6390d465af
Mateussantos1207 <mateussntos99@gmail.com>
2025-09-23 18:37:23 -0300

    adicionei a lista de pokemons

--- Poke.js
+++ Poke.js
@@ -1,17 +1,24 @@
-const lista_pokemons = [
-  { nome: "Pikachu", tipo: "Elétrico", nivel: 25, hp: 120, gen: 1 },
-  { nome: "Charmander", tipo: "Fogo", nivel: 20, hp: 110, gen: 1 },
-  { nome: "Squirtle", tipo: "Água", nivel: 22, hp: 115, gen: 1 },
-  { nome: "Bulbasaur", tipo: "Planta", nivel: 18, hp: 105, gen: 1 },
-  { nome: "Jigglypuff", tipo: "Normal", nivel: 28, hp: 130, gen: 1 },
-  { nome: "Onix", tipo: "Pedra", nivel: 30, hp: 160, gen: 1 },
-  { nome: "Mewtwo", tipo: "Psíquico", nivel: 70, hp: 320, gen: 1 },
-  { nome: "Chikorita", tipo: "Planta", nivel: 18, hp: 100, gen: 2 },
-  { nome: "Cyndaquil", tipo: "Fogo", nivel: 20, hp: 105, gen: 2 },
-  { nome: "Totodile", tipo: "Água", nivel: 22, hp: 115, gen: 2 }
-];
+document.addEventListener("DOMContentLoaded", () => {
+    const lista_pokemons = [
+      { nome: "Pikachu", tipo: "Elétrico", nivel: 25, hp: 120, gen: 1 },
+      { nome: "Charmander", tipo: "Fogo", nivel: 20, hp: 110, gen: 1 },
+      { nome: "Squirtle", tipo: "Água", nivel: 22, hp: 115, gen: 1 },
+      { nome: "Bulbasaur", tipo: "Planta", nivel: 18, hp: 105, gen: 1 },
+      { nome: "Jigglypuff", tipo: "Normal", nivel: 28, hp: 130, gen: 1 },
+      { nome: "Onix", tipo: "Pedra", nivel: 30, hp: 160, gen: 1 },
+      { nome: "Mewtwo", tipo: "Psíquico", nivel: 70, hp: 320, gen: 1 },
+      { nome: "Chikorita", tipo: "Planta", nivel: 18, hp: 100, gen: 2 },
+      { nome: "Cyndaquil", tipo: "Fogo", nivel: 20, hp: 105, gen: 2 },
+      { nome: "Totodile", tipo: "Água", nivel: 22, hp: 115, gen: 2 }
+    ];
 
-const lista_pokemonsJSON = JSON.stringify(lista_pokemons)
-const lista_pokemonsPARSE = JSON.parse(lista_pokemonsJSON)
-console.log(lista_pokemonsJSON)
-console.log(lista_pokemonsPARSE)
+    const ul = document.getElementById("listaPokemons");
+
+    ul.innerHTML = "";
+
+    lista_pokemons.forEach(pokemon => {
+        const li = document.createElement("li");
+        li.textContent = `${pokemon.nome} | Tipo: ${pokemon.tipo} | Nível: ${pokemon.nivel} | HP: ${pokemon.hp}`;
+        ul.appendChild(li);
+    });
+});
